9d4fd01baaa84fc0e09684db78f29cfe0bae1b6a
poggonet01 <artiompogonet98@gmail.com>
2021-06-14 17:32:26 +0300

    Addes some mock request without database

--- CarProject/src/main/java/org/acme/Car.java
+++ CarProject/src/main/java/org/acme/Car.java
@@ -15,19 +15,30 @@ public class Car extends PanacheEntityBase {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public Long Id;
+	public Long id;
 	
 	@Column(name = "CarName" , nullable= false) 
-	public String Name;
+	public String name;
 	
 	@Column(name = "CarColor" , length = 10 , nullable = false)
-	public String Color;
+	public String color;
 	
 	public Car () {}
 	public Car(Long Id ,String name, String color) {
-		this.Id = Id;
-		this.Name = name;
-		this.Color = color;
+		this.id = Id;
+		this.name = name;
+		this.color = color;
+	}
+	
+	public Car findCarById(Long id) {
+		return findById(id);
+	}
+	
+	public Car findByName(String name) {
+		return find("name" , name).firstResult();
+	}
+	public void deleteCarByName(String name) {
+		delete("name", name);
 	}
 		
 
--- CarProject/src/main/java/org/acme/ExampleResource.java
+++ CarProject/src/main/java/org/acme/ExampleResource.java
@@ -1,16 +1,38 @@
 package org.acme;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/hello")
+@Path("/v1/cars")
 public class ExampleResource {
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy Reactive";
-    }
+	public static List<String> cars = new ArrayList<String>();
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response getCars() {
+		return Response.ok(cars).build();
+	}
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/size")
+	public Integer countCars() {
+		return (int) Car.count();
+	}
+	
+	@POST
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response addCar(String newCar) {
+		cars.add(newCar);
+		return Response.ok(cars).build();
+	}
+	
 }
