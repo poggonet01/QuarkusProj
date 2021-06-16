75cea905fab026717047cb68f698d33672d5bda6
poggonet01 <artiompogonet98@gmail.com>
2021-06-16 16:48:22 +0300

    Add a PUT request to add cars in garage

--- CarProject/src/main/java/org/acme/CarController.java
+++ CarProject/src/main/java/org/acme/CarController.java
@@ -10,6 +10,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -47,6 +48,17 @@ public class CarController {
 		car.name = newCar.name;
 		car.persist();
 		return Response.ok(car).build();
+	}
+	
+	@PUT
+	@Transactional
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/modify")
+	public Response changeColor(@QueryParam("id") long id , @QueryParam("color") String color) {
+		Car newCar = Car.findCarById(id);
+		newCar.color = color;
+		newCar.persist();
+		return Response.ok(newCar).build();
 	}
 	
 	@DELETE
