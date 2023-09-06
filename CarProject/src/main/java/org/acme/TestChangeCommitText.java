bc03604fddfd9644d06abb54a1a5cbd880b0622f
poggonet01 <artiompogonet98@gmail.com>
2023-09-06 18:04:06 +0300

    commit which has to be changed1

---
+++ CarProject/src/main/java/org/acme/TestChangeCommitText.java
@@ -0,0 +1,6 @@
+package org.acme;
+
+public class TestChangeCommitText {
+    private static final String PARAM = "TEXT_TO_BE_CHANGED1";
+    private static final String PARAM2 = "TEXT_TO_BE_CHANGED1";
+}
